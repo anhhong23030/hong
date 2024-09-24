a0b89afb974dbcd4852c087e279f7be2bef95dca
hong <anhn23030@gmail.com>
2024-09-24 09:13:21 +0700

    demo git

--- src/main/java/com/laptrinh2a/git/App.java
+++ src/main/java/com/laptrinh2a/git/App.java
@@ -2,6 +2,6 @@ package com.laptrinh2a.git;
 
 public class App {
     public static void main(String[] args){
-        System.out.println("Hello minh la hong,ahihi");
+        System.out.println("Hello minh la hong,ahihi đồ ngốc");
     }
 }
